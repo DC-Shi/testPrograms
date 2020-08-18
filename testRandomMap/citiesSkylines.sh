60f4d9a85ce9eeff8feeee2ce61ebab62ed19528
dc-shi <daochen.shi@gmail.com>
2020-08-18 19:02:27 +0800

    Refactor code for random map

--- testRandomMap/citiesSkylines.sh
+++ testRandomMap/citiesSkylines.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# All maps
-declare -a maps=("Black Woods" "Cliffside Bay" "Diamond Coast" "Foggy Hills" "Grand River" "Green Plains" "Islands" "Lagoon Shore" "Riverrun" "Sandy Beach" "Shady Strands" "Two Rivers")
+# All base maps
+declare -a maps
 
+
+for mapfile in *.mapnames
+do
+  curPrefix="${mapfile%%.*}"
+  while read line;
+  do
+    
+    maps+=("$curPrefix $line")
+  done <$mapfile
+done 
 count="${#maps[@]}"
 
 # Print 5 random maps
 for i in 1 2 3 4 5
 do
   cur="$(( $RANDOM % $count ))"
-  echo "[$((cur+1))] ${maps[$cur]}"
+  echo "${maps[$cur]}"
 done
