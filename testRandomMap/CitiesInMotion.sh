7af3dffa3c2356182ed58bdfa2694344907f7794
DC-Shi <daochen.shi@gmail.com>
2021-01-18 12:05:23 +0800

    Add Cities In Motion maps

---
+++ testRandomMap/CitiesInMotion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# All base maps
+declare -a maps
+
+
+for mapfile in CIM*.mapnames
+do
+  curPrefix="${mapfile%%.*}"
+  while read line;
+  do
+    
+    maps+=("$curPrefix $line")
+  done <$mapfile
+done 
+count="${#maps[@]}"
+
+# Print 5 random maps
+for i in 1 2 3 4 5
+do
+  cur="$(( $RANDOM % $count ))"
+  echo "${maps[$cur]}"
+done
