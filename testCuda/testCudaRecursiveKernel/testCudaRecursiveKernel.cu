e503fe849e94a707e382947b3b373d661701a66b
DC-Shi <daochen.shi@gmail.com>
2022-04-07 06:29:29 +0000

    Update test case 2 and 3: global-device invokation and global-global invokation.

--- testCuda/testCudaRecursiveKernel/testCudaRecursiveKernel.cu
+++ testCuda/testCudaRecursiveKernel/testCudaRecursiveKernel.cu
@@ -30,12 +30,79 @@ cudaError_t runTest1()
     return ret;
 }
 
+////////////////////// Test 2
+__device__ float test2_d_f(int v)
+{
+    float tmp = 0;
+    tmp = 1.0 /(1+exp(v*1.0));
+    return tmp;
+}
+
+
+
+__global__ void test2_kernel()
+{
+    int idx = blockIdx.x*gridDim.x + threadIdx.x;
+    float val = test2_d_f(idx);
+    printf("kernel (%d,%d) calculated %f\n", blockIdx.x, threadIdx.x, val);
+}
+
+cudaError_t runTest2()
+{
+    test2_kernel<<<2,3>>>();
+    cudaError_t ret = cudaDeviceSynchronize();
+    gpuErrchk("host process", ret);
+    
+    return ret;
+}
+
+
+////////////////////// Test 3
+__device__ float val3[10];
+__global__ void test3_g_f(int v, float* ret, int idx)
+{
+    ret[idx] = 2.0 /(1.0+exp(v*1.0));
+}
+__global__ void test3_kernel()
+{
+    int idx = blockIdx.x*gridDim.x + threadIdx.x;
+    test3_g_f<<<1,1>>>(idx, val3, idx);
+
+    // k1<<<10,1>>>(val3, newidx, offset);
+    // cudaDeviceSynchronize();
+    // k2<<<>>>(val3);
+    // k3<<<>>>(val3);
+    // k4<<<>>>(val3);
+    cudaDeviceSynchronize();
+    printf("kernel3 (%d,%d) calculated %f\n", blockIdx.x, threadIdx.x, val3[idx]);
+}
+
+cudaError_t runTest3()
+{
+    test3_kernel<<<2,3>>>();
+
+
+    // k1<<<>>>();
+    // k2<<<>>>();
+    // k3<<<>>>();
+    // k4<<<>>>();
+    cudaError_t ret = cudaDeviceSynchronize();
+    gpuErrchk("host process", ret);
+    
+    return ret;
+}
+
 int main()
 {
     printf("=====================================\n");
     
-    if (runTest1() != cudaSuccess)
-        printf ("xxxFAILED Test 1\n");
+    if (runTest2() != cudaSuccess)
+        printf ("xxxFAILED Test 2\n");
     else
-        printf ("SUCCEEDED Test 1\n");
+        printf ("SUCCEEDED Test 2\n");
+    if (runTest3() != cudaSuccess)
+        printf ("xxxFAILED Test 3\n");
+    else
+        printf ("SUCCEEDED Test 3\n");
+
 }
