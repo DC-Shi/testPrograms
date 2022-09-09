4006e787ce6a38db5d5916ffbb6b6ba639a73f21
DC-Shi <daochen.shi@gmail.com>
2022-09-09 07:12:02 +0000

    Add C++ style GPU errorcheck

--- testCuda/inc/cudaheader.cuh
+++ testCuda/inc/cudaheader.cuh
@@ -12,4 +12,23 @@ inline void gpuAssert(const char *prompt, cudaError_t code, const char *file, in
    else
        printf("success : %s\n", prompt);
 }
+
+// Used for C++ functions
+#ifdef __cplusplus
+#include <iostream>
+#define gpuErrchkCpp20(str, ans) { gpuAssertCpp20((str), (ans), __FILE__, __LINE__); }
+inline void gpuAssertCpp20(const char *prompt, cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      std::cerr << "GPUASSERT "<< prompt << " Failed:" << cudaGetErrorString(code) << file << line << std::endl;;
+      if (abort) exit(code);
+   }
+   else
+   // Only print success if prompt is not empty
+      if ( prompt[0] != 0 || prompt != nullptr || prompt != NULL)
+         std::cout << "success : " << prompt << std::endl;
+}
+#endif
+
 #endif
