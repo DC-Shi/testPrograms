87cefe9b9226cc03ff9ba201fc6c81bbb2ba1ff9
DC-Shi <daochen.shi@gmail.com>
2025-01-22 21:22:50 +0800

    add compiler test code

---
+++ testCompilers/uint.c
@@ -0,0 +1,18 @@
+// The code came from Dave W Plummer
+// https://x.com/davepl1968/status/1881713879916720420
+//
+#include <stdio.h>
+
+int main()
+{
+  int x = -10;
+  unsigned int y = 5;
+  if (x > y)
+  {
+    printf("x is greater than y\n");
+  }
+  else
+  {
+    printf("x is less than y\n");
+  }
+}
